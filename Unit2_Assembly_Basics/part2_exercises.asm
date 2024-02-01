ec6d2b232819dced8dec3e534bdbf510a704079c
Megan Avery <mavery3@stedwards.edu>
2024-02-01 21:07:28 +0000

    initial part 2 commit

--- Unit2_Assembly_Basics/part2_exercises.asm
+++ Unit2_Assembly_Basics/part2_exercises.asm
@@ -10,12 +10,29 @@
 
 %include "asm_io.inc"
 
+age equ 30 ; symbol
+%define fav_number 14 ; macro
+
 ; initialized data
 segment .data
+fav_color db "purple 💜", 0 ; string
+least_fav_color db "yellow", 0
+greeting db "Hello World!", 0
+
+char_prompt db "Enter a character: ", 0
+int_prompt db "Enter an integer: ", 0
+
+number dd 116  ; integer
+letter db "A"  ; character
+first_letter db "R"
+first_letter_ascii dd 82
+
+many_numbers times 15 dd 34
+characters times 5 db "Z"
 
 ; uninitialized data
 segment .bss
-
+space_for_int resd 1 ; space for 1 integer
 
 segment .text
         global  asm_main
@@ -23,8 +40,41 @@ asm_main:
         enter   0,0               ; setup routine
         pusha
 
-		; TODO: add code
+        dump_mem 1, characters, 0
+        call print_nl
+        dump_mem 2, characters + 3, 0
 
+	; mov eax, [number] ; eax = 116
+        ; mov dword [number], 32
+
+        ; ; mov eax, [number]
+        ; call print_int
+        ; call print_char
+
+        ; mov eax, [first_letter_ascii]
+        ; call print_int
+        ; call print_nl
+        ; call print_char
+
+        ; dump_mem 1, fav_color, 0
+
+        ; mov eax, fav_color + 3
+        ; call print_string
+
+        ; mov eax, greeting
+        ; call print_string
+        ; call print_nl
+        ; dump_mem 1, greeting, 0
+
+        mov eax, int_prompt
+        call print_string
+        call read_int
+
+        mov eax, char_prompt
+        call print_string
+        call read_char
+
+        
         popa
         mov     eax, 0            ; return back to C
         leave                     
