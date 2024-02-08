ecdb59861d2df00195ab052defc2f347a20a24e6
Megan Avery <mavery3@stedwards.edu>
2024-02-08 21:01:20 +0000

    initial commit

--- Unit3_Integers_And_Ops/shifts_exercises.asm
+++ Unit3_Integers_And_Ops/shifts_exercises.asm
@@ -21,7 +21,58 @@ asm_main:
         enter   0,0               ; setup routine
         pusha
 
-		; TODO: add code
+	; mov eax, 10
+        ; shl eax, 3
+        ; call print_int
+        
+        ; mov ebx, 2
+        ; shl eax, ebx
+
+        ; mov eax, 080000045H
+        ; call print_int
+        ; call print_nl
+
+        ; shl eax, 1
+        ; call print_int
+
+        ; mov eax, 23
+        ; shr eax, 2
+        ; call print_int
+
+        ; mov eax, 4 ; 0100
+        ; mov ebx, 2 ; 0010
+        ; test eax, ebx
+        ; dump_regs 1
+
+        ; zero out register
+        ; shift until empty
+        ; mov eax, 190
+        ; shl eax, 100
+        ; call print_int
+        ; mov eax, 13
+        ; xor eax, eax
+        ; call print_int
+
+        ; mov ebx, 1
+        ; shl ebx, 2
+
+        ; mov eax, 11
+        ; or eax, ebx
+        ; call print_int
+
+        ; mov ebx, 1
+        ; shl ebx, 3
+        ; not ebx
+
+        ; mov eax, 15
+        ; and eax, ebx
+        ; call print_int
+
+        mov ebx, 00000FFFFH
+        mov eax, -1
+
+        and eax, ebx
+        dump_regs 1
 
         popa
         mov     eax, 0            ; return back to C
