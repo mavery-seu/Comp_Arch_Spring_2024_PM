1b8adf86ce4325d453c5e0be7ce459799f10596c
Megan Avery <mavery3@stedwards.edu>
2024-02-28 10:19:06 +0000

    loop exercises and commenting on number_guessing_game

--- Unit4_Control_Structures/loops_exercises.asm
+++ Unit4_Control_Structures/loops_exercises.asm
@@ -4,8 +4,15 @@
 ; Purpose - to learn about loops in NASM
 
 %include "asm_io.inc"
-segment .data						; initialized data
+segment .data
+rocket db "🚀!", 0
 
+perfect_prompt db "Enter a power of 2: ", 0
+perfect db "PERFECT", 0
+another_perfect_prompt db "Again! ", 0
+
+fear_the_goat db "Fear the 🐐!", 0
+reaction db "Reaction? ", 0
 
 segment .bss						; uninitialized data
 
@@ -16,11 +23,65 @@ asm_main:
         enter   0,0               	; setup routine
         pusha
 
-		; TODO: FOR EXERCISE
+	; FOR LOOP EXERCISE
+        mov ecx, 5              ; indicates loop should run 5 times
+for:                            ; label for "top" of for loop
+        mov eax, ecx            ; move ecx into eax
+        call print_int          ; print eax
+        call print_nl           ; print a newline so each int on its own line
 
-		; TODO: WHILE EXERCISE
+        ; if update ecx inside loop like this will get an infinite loop
+        ; mov ecx, 5    
 
-		; TODO: DO WHILE EXERCISE
+        loop for                ; decrement ecx, if ecx != 0 jump back to for label     
+
+        mov eax, rocket         ; move rocket into eax
+        call print_string       ; print rocket
+        call print_nl           ; print a newline
+        call print_nl           ; print a newline
+	
+        ; WHILE EXERCISE
+
+        ; setup
+        mov eax, perfect_prompt ; move perfect_prompt into eax
+        call print_string       ; print perfect_prompt
+        call read_int           ; read and integer from the user
+while:                          ; label for the "top" of the while loop
+        mov ebx, eax            ; move eax into ebx
+        dec ebx                 ; decrement ebx, now ebx = eax - 1
+        and eax, ebx            ; and eax with ebx, store result in eax
+        cmp eax, 0              ; compare eax to 0
+        
+        ; if missing causes aninfinite loop
+        jnz endwhile            ; if zero flag is NOT set skip over body of loop          
+
+        mov eax, perfect        ; move perfect into eax
+        call print_string       ; print perfect
+        call print_nl           ; print a newline
+        
+        ; update - if missing from while loop get an infinite loop
+        mov eax, another_perfect_prompt ; move another_perfect_prompt into eax
+        call print_string       ; print another_perfect_prompt
+        call read_int           ; read an integer from the user
+
+        jmp while               ; unconditionally jump to the while label
+endwhile:                       ; label for the end of the while loop
+        call print_nl           ; print a newline
+
+	;DO WHILE EXERCISE
+do:                             ; label for the "top" of the do while loop
+        call read_char          ; read a character, to consume newline characters
+        mov eax, fear_the_goat  ; move fear_the_goat into eax
+        call print_string       ; print fear_the_goat
+        call print_nl           ; print a newline
+
+        mov eax, reaction       ; move reaction into eax
+        call print_string       ; print reaction
+        call read_char          ; read a character from the user
+
+        cmp eax, "!"            ; compare the character to !
+        jne do                  ; conditonally jump if eax and ! are NOT equal
+
 
         popa
         mov     eax, 0            ; return back to C
--- Unit4_Control_Structures/number_guessing_game.asm
+++ Unit4_Control_Structures/number_guessing_game.asm
@@ -7,10 +7,12 @@ segment .data
 ;
 ; initialized data is put in the data segment here
 ;
+; constants
 SECRET_NUMBER equ 67
 NUMBER_MAX equ 100
 NUMBER_MIN equ 0
 
+; strings
 guess_prompt db "Guess a number between ", 0
 and_prompt   db " and ", 0
 colon_prompt db ": ", 0
@@ -23,7 +25,7 @@ segment .bss
 ;
 ; uninitialized data is put in the bss segment
 ;
-guess resd 1
+guess resd 1                    ; variable to hold user's guess
 
 segment .text
         global  asm_main
@@ -31,10 +33,13 @@ asm_main:
         enter   0,0               ; setup routine
         pusha
 
-; CODE START
-        mov bh, NUMBER_MAX
-        mov bl, NUMBER_MIN
+; CODE BEGIN
 
+        ; starting points for boundaries of guesses
+        mov bh, NUMBER_MAX      ; move the NUMBER_MAX into bh (higher 8 bits of AX)
+        mov bl, NUMBER_MIN      ; move the NUMBER_MIN ito bl (lower 8 bits of AX/EAX)
+
+        ; print out "Enter a number between {NUMBER_MIN} and {NUMBER_MAX}"
         mov eax, guess_prompt
         call print_string
         mov eax, NUMBER_MIN
@@ -45,37 +50,44 @@ asm_main:
         call print_int
         mov eax, colon_prompt
         call print_string
+        ; end of print block
         
-get_guess:
-        call read_int
+get_guess:                      ; label for the "top" of the elongated while loop
+        call read_int           ; read an integer from the user
 
-        mov [guess], eax
+        mov [guess], eax        ; move the user's input into guess variable
 
-        cmp [guess], bh
-        jg out_of_range
-        cmp [guess], bl
-        jl out_of_range
+        cmp [guess], bh         ; compare the user's input to to the higher boundary
+        jg out_of_range         ; if greater than the higher boundary jump to out_of_range
+        cmp [guess], bl         ; compare user's input to the lower boundary
+        jl out_of_range         ; if less than the lower boundary jump to out_of_range
         
-        cmp BYTE [guess], SECRET_NUMBER
-        je guessed_correctly
-        jl guess_too_small
+        cmp BYTE [guess], SECRET_NUMBER ; compare the user's input to the SECRET_NUMBER
+        je guessed_correctly    ; if user guessed the secret number jump to guessed_correctly
+        jl guess_too_small      ; if user's guess is less than the SECRET_NUMBER jump to guess_to_small
 
-guess_too_big:
-        mov bh, [guess]
-        dec bh
-        mov eax, too_big_prompt
-        call print_string
-        jmp get_guess
+guess_too_big:                  ; label for guess_to_big, only here for show, fall through to this point
+        ; if the user guessed is too big know that any number >= that number isn't a possiblity
+        ; update the higher boundary of the guesses
+        mov bh, [guess]         ; move the user's guess into bh (the higher boundary)
+        dec bh                  ; decrement the higher boundary
+       
+        mov eax, too_big_prompt ; move too_big_prompt into eax
+        call print_string       ; print too_big_prompt
+        jmp get_guess           ; unconditionally jump to get_guess/loop back to the top
 
-guess_too_small:
-        mov bl, [guess]
-        inc bl
-        mov eax, too_small_prompt
-        call print_string
-        jmp get_guess
+guess_too_small:                ; label for guess_too_small
+        ; if the user guessed is too small know that any number <= that number isn't a possiblity
+        ; update the lower boundary of the guesses
+        mov bl, [guess]         ; move user's guess into bl (the lower boundary)
+        inc bl                  ; increment the lower boundary
+        mov eax, too_small_prompt ; move too_small_prompt into eax
+        call print_string       ; print too_small_prompt
+        jmp get_guess           ; unconditionally jump to get_guess/loop back to the top
 
-out_of_range:
-        mov eax, out_of_range_output
+out_of_range:                   ; label for out_of_range
+        ; print out "Not in range! Must be between {bl} and {bh}:"
+        mov eax, out_of_range_output 
         call print_string
         mov eax, 0
         mov al, bl
@@ -87,12 +99,14 @@ out_of_range:
         call print_int
         mov eax, colon_prompt
         call print_string
+        ; end of print block
 
-        jmp get_guess
+        jmp get_guess           ; unconditionally jump back to get_guess/loop back to the top
 
-guessed_correctly:
-        mov eax, correct_output
-        call print_string
+guessed_correctly:              ; label for guessed_correct
+        mov eax, correct_output ; move correct_output into eax
+        call print_string       ; print_correct_output
+        ; no jump back to get_guess because this is outside of the while loop
 
 ; CODE END
                 
