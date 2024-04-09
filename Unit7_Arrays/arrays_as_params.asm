f2757e132bfc082674f54e52ee2a4d8e4b5ccaa6
Megan Avery <mavery3@stedwards.edu>
2024-04-09 20:16:57 +0000

    updating array basics code

--- Unit7_Arrays/arrays_as_params.asm
+++ Unit7_Arrays/arrays_as_params.asm
@@ -25,16 +25,17 @@ segment .bss
 ; uninitialized data is put in the bss segment
 ;
 
-
 segment .text
         global  asm_main
 asm_main:
         enter   0,0               ; setup routine
         pusha
 
+		; print message about the original array
 		mov eax, original_message
 		call print_string
 
+		; print the original array for the first time
 		push NUMBERS_LENGTH
 		push numbers
 		call print_array_simple
@@ -42,16 +43,20 @@ asm_main:
 
 		call print_nl
 
+		; get number to multiply each element in array by
 		mov eax, multiply_prompt
 		call print_string
 		call read_int
 
+		; call subprogram to multiply each element in the array
+		; by the factor given by the user
 		push eax
 		push NUMBERS_LENGTH
 		push numbers
 		call multiply_array_elements
 		add esp, 12
 
+		; print out multiplied array
 		mov eax, multiplied_message
 		call print_string
 
@@ -62,16 +67,19 @@ asm_main:
 		
 		call print_nl
 
+		; get amount to rotate shift left by
 		mov eax, shift_prompt
 		call print_string
 		call read_int
 
+		; call subprogram to rotate shift the array
 		push eax
 		push NUMBERS_LENGTH
 		push numbers
 		call rotate_shift_array_left
 		add esp, 12
 
+		; print out the shifted array
 		mov eax, shifted_message
 		call print_string
 
@@ -85,102 +93,159 @@ asm_main:
         leave                     
         ret
 
+; Subprogram to multiply every element in an int array 
+; by a given factor
+;
+; method header: multiply_array_elements(arr, arr_size, factor)
+;
+; Stack Frame:
+; ------------
+; EBP + 16      paremter 3: the factor to multiply by
+; EBP + 12      paremter 2: size of the array
+; EBP + 8       paremter 1: the address of the first element in the array
+; EBP + 4       return address
+; EBP           previous EBP
 multiply_array_elements:
+		; prologue
 		enter 0, 0
+
+		; save off ESI and EBX 
 		push esi
 		push ebx
 
-		xor esi, esi
-		mov ecx, [ebp + 12]
-		mov ebx, [ebp + 8]
+		xor esi, esi		; 0 out the source index
+		mov ecx, [ebp + 12]	; move the size of the array into ECX
+		mov ebx, [ebp + 8]	; ove the starting address into EBX
 
+; for loop to run over each element in the array and multiply it by the factor
 multiply_loop:
-		mov eax, dword [ebx + BYTES_IN_INT * esi]
-		imul eax, [ebp + 16]
+		mov eax, dword [ebx + BYTES_IN_INT * esi]	; move the current value of arr[i] into EAX
+		imul eax, [ebp + 16]						; multiply value of arr[i] by factor
 
-		mov dword [ebx + BYTES_IN_INT * esi], eax
+		mov dword [ebx + BYTES_IN_INT * esi], eax	; arr[i] = updated value
 
-		inc esi
-		loop multiply_loop
+		inc esi										; add one to ESI to move down one spot in the array
+		loop multiply_loop							; loop back to the top
 
+		; restore ESI and EBX
 		pop ebx
 		pop esi
+
+		; epilogue
 		leave
 		ret
 
+; Subprogram to rotate shift the given array left by the given amount
+;
+; method header: rotate_shift_array_left(arr, arr_size, rotate_amount)
+;
+; Stack Frame:
+; ------------
+; EBP + 16      paremter 3: the amount to rotate left by
+; EBP + 12      paremter 2: size of the array
+; EBP + 8       paremter 1: the address of the first element in the array
+; EBP + 4       return address
+; EBP           previous EBP
+; EBP - 4		placeholder for the first element in the array
 rotate_shift_array_left:
+		; prologue - making room for 1 local variable
 		enter 4, 0
+
+		; save off ESI, EDI, and EBX
 		push esi
 		push edi
 		push ebx
-
 		
-		mov ecx, [ebp + 16]
+		mov ecx, [ebp + 16]		; move amount to rotate into ECX
 outer_loop:
-		push ecx
+		push ecx				; save off outer value of ECX
 
-		mov esi, 1
+		mov esi, 1				; move 1 into the source index (ESI) (i = 1)
 
-		mov ecx, [ebp + 12]
-		dec ecx
+		mov ecx, [ebp + 12]		; move the size of the array into ECX
+		dec ecx					; ECX-- 
 
-		mov ebx, [ebp + 8]
+		mov ebx, [ebp + 8]		; move the address of the first element into EBX
 
-		mov eax, [ebx]
-		mov [ebp - 4], eax
+		mov eax, [ebx]			; move the value at arr[0] into EAX
+		mov [ebp - 4], eax		; save the value of arr[0] into the local variable
 
+; innner loop to shift every element to the left one spot
+; will run size of array - 1 times
 shift_loop:
-		mov edi, esi
-		dec edi
+		mov edi, esi			; move the source index (ESI) into the destination index (EDI)
+		dec edi					; EDI--, to get ready to shift to the left
 
-		mov eax, [ebx + BYTES_IN_INT * esi]
-		mov [ebx + BYTES_IN_INT * edi], eax
+		mov eax, [ebx + BYTES_IN_INT * esi]	; EAX = arr[i]
+		mov [ebx + BYTES_IN_INT * edi], eax	; arr[i - 1] = arr[i]
 
-		inc esi
-		loop shift_loop
+		inc esi								; ESI++ (i++)
+		loop shift_loop						; loop back to the top of the inner loop
 
-		mov eax, [ebp + 8]
-		mov ebx, [ebp + 12]
-		imul ebx, BYTES_IN_INT
-		sub ebx, BYTES_IN_INT
+		mov eax, [ebp + 8]					; EAX = address of first element 
+		mov ebx, [ebp + 12]					; EBX = size of the array
+		imul ebx, BYTES_IN_INT				; EBX *= 4
+		sub ebx, BYTES_IN_INT				; EBX -= 4 
 
-		add eax, ebx
+		add eax, ebx						; EAX is the address of the last element in the array
  
-		mov ebx, [ebp - 4]
-		mov [eax], ebx
+		mov ebx, [ebp - 4]					; move local variable (the original first element in the array) into EBX
+		mov [eax], ebx						; move EBX into value at EAX, move original first element to the end of the array
 
-		pop ecx
-		loop outer_loop
+		pop ecx								; restore ECX to its value for the outer loop
+		loop outer_loop						; loop back to the top of the outerloop
 
+		; restore EBX, EDI, and ESI
 		pop ebx
 		pop edi 
 		pop esi
+
+		; epilogue
 		leave
 		ret
 
+; Subprogram to print the contents of an integer array
+; numbers separated by spaces
+;
+; method header: print_array_simple(arr, arr_size)
+;
+; Stack Frame:
+; ------------
+; EBP + 12      paremter 2: size of the array
+; EBP + 8       paremter 1: the address of the first element in the array
+; EBP + 4       return address
+; EBP           previous EBP
 print_array_simple:
+		; progogue
 		enter 0, 0
+
+		; storing off ESI and EBX
 		push esi
 		push ebx
 
-		xor esi, esi
-		mov ecx, [ebp + 12]
-		mov ebx, [ebp + 8]
+		xor esi, esi				; zero out the source index (i)
+		mov ecx, [ebp + 12]			; move size of array into ECX (loop counter)
+		mov ebx, [ebp + 8]			; move address of first element in array into EBX
 
+; loop to print out each element in the array then a space
 print_loop:
-		mov eax, dword [ebx + BYTES_IN_INT * esi]
-		call print_int
+		mov eax, dword [ebx + BYTES_IN_INT * esi]	; move arr[i] into EAX
+		call print_int								; print arr[i]
 
-		mov eax, space
+		; print a space
+		mov eax, space	
 		call print_string
 
-		inc esi
-		loop print_loop
+		inc esi										; i++
+		loop print_loop								; loop back to the top of the for loop
 
-		call print_nl
+		call print_nl								; print a newline 
 
+		; restore EBX and ESI
 		pop ebx
 		pop esi
+
+		; epilogue
 		leave
 		ret
 		
--- Unit7_Arrays/two_d_arrays.asm
+++ Unit7_Arrays/two_d_arrays.asm
@@ -30,129 +30,181 @@ segment .bss
 segment .text
         global  asm_main
 asm_main:
+		; prologue
         enter   0,0               ; setup routine
         pusha
 
+		; print message about the original matrix
 		mov eax, original_matrix
 		call print_string
 		call print_nl
 
+		; print the original matrix
 		push dword [matrix_cols]
 		push dword [matrix_rows]
 		push matrix
 		call print_matrix
 		add esp, 12
 
+		; print the update question and get answer
 		call print_nl
 		mov eax, update_question
 		call print_string
 		call read_char
 
+		; consume extra newline character
 		mov ebx, eax
 		call read_char
+; while to keep going as long as the user enters a lowercase y
 update_while:
 		cmp ebx, "y"
-		jne end_while
+		jne end_while				; jump to the end if the user doesn't enter a y
 
-		call print_nl
+		call print_nl				; print a newline character
 
+		; call update_matrix
 		push dword [matrix_cols]
 		push matrix
 		call update_matrix
 		add esp, 8
 
+		; consume newline character
 		call read_char
 		call print_nl
 
+		; print the updated matrix
 		push dword [matrix_cols]
 		push dword [matrix_rows]
 		push matrix
 		call print_matrix
 		add esp, 12
 
+		; ask again if the user wants to update the matrix
 		call print_nl
 		mov eax, update_question
 		call print_string
-		call read_char
+		call read_char			; get answer
 
+		; consume the newline character
 		mov ebx, eax
 		call read_char
 
+		; unconditional jump back to the top of the while loop
 		jmp update_while
 
+; the label for the end of the while loop
 end_while:
 
+		; epilogue
         popa
         mov     eax, 0            ; return back to C
         leave                     
         ret
 
+; Subprogram to update a matrix at a position gotten from the user
+;
+; method header: update_matrix(arr, num_cols)
+;
+; Stack Frame:
+; ------------
+; EBP + 12      paremter 2: the number of columns in the matrix
+; EBP + 8       paremter 1: the address of the first element in the matrix
+; EBP + 4       return address
+; EBP           previous EBP
 update_matrix:
+		; prologue
 		enter 0, 0
+
+		; storing off ESI and EBX
 		push esi 
 		push ebx
 		
+		; get the row to update
 		mov eax, row_question
 		call print_string
 		call read_int
 
-		mov ebx, eax	
+		mov ebx, eax				; save off row to update
 
+		; get the column to update
 		mov eax, col_question
 		call print_string
 		call read_int
 
-		mov ecx, eax		
+		mov ecx, eax				; save off column to update
 
+		; get the new value for the matrix stop
 		mov eax, new_int_question
 		call print_string
 		call read_int
 
-		mov edx, [ebp + 8]
+		mov edx, [ebp + 8]			; move the address of matrix[0][0] into EDX
 		
-		imul ebx, [ebp + 12]
-		imul ebx, BYTES_IN_INT
-		add edx, ebx
+		imul ebx, [ebp + 12]		; multiple EBX (the row) by the number of columns (N)
+		imul ebx, BYTES_IN_INT		; multiple that by 4
+		add edx, ebx				; add N * row * 4 to the address of matrix[0][0]
 
-		imul ecx, BYTES_IN_INT
-		add edx, ecx
+		imul ecx, BYTES_IN_INT		; multiply the column by 4
+		add edx, ecx				; add the column * 4 to address of matrix[0][0] + N * row * 4
 
-		mov [edx], eax
+		mov [edx], eax				; move the new value into matrix[row][col]
 
+		; restore EBX and ESI
 		pop ebx
 		pop esi
+
+		; epilogue
 		leave
 		ret
 
+; Subprogram to print out the contents of an in matrix
+;
+; method header: print_matrix(arr, num_rows, num_cols)
+;
+; Stack Frame:
+; ------------
+; EBP + 16      paremter 3: the number of columns in the matrix
+; EBP + 12      paremter 2: the number of rows in the matrix
+; EBP + 8       paremter 1: the address of the first element in the matrix
+; EBP + 4       return address
+; EBP           previous EBP
 print_matrix:
+		; prologue
 		enter 0, 0
+
+		; save off ESI and EBX
 		push esi
 		push ebx
 
-		xor esi, esi
-		mov ebx, [ebp + 8]
-		mov ecx, [ebp + 12]
+		xor esi, esi				; zero out the source index (i)
+		mov ebx, [ebp + 8]			; move the address of matrix[0][0] into EBX
+		mov ecx, [ebp + 12]			; move the number of rows into ECX
+; loop that runs "rows" times
 row_loop:
-		push ecx
-		mov ecx, [ebp + 16]
+		push ecx					; save off outer loop's value fo ECX
+		mov ecx, [ebp + 16]			; move the number of columns into ECX
+; loop that runs "cols" times
 col_loop:
-		mov eax, [ebx + BYTES_IN_INT * esi]
-		call print_int
+		mov eax, [ebx + BYTES_IN_INT * esi]	; move matrix[i] into EAX, treating matrix like one long 1D array
+		call print_int						; print matrix[i]
 
+		; print a space
 		mov eax, " "
 		call print_char
 
-		inc esi
-		loop col_loop
+		inc esi						; i++
+		loop col_loop				; jump back to the top of the inner loop
 
-		call print_nl
+		call print_nl				; print a newline character
 
-		pop ecx
-		loop row_loop
+		pop ecx						; restore outer loop's value of ECX from before
+		loop row_loop				; loop back to the top of the outer loop
 
-
+		; restore EBX and ESI
 		pop ebx
 		pop esi
+
+		; epilogue
 		leave
 		ret
 
